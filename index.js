18ca276682dec77623c615a0366f3fe2beec93fc
Mario Castro <mcast032@gmail.com>
2024-06-26 17:21:37 -0700

    done

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function isPalindrome(word) {
+  const wordBackwords = word.split('').reverse().join('');
+  return word === wordBackwords;
   // Write your algorithm here
 }
 
+
 /* 
   Add your pseudocode here
+
+initialize a backwords string called wordBackords.
+split the word up into individual letters in an array. then revers the order of the arry. then join the new reversed array and make it into a string.
+finally return the original word and compare it to the reversed word.
 */
+
+// Test cases
+
 
 /*
   Add written explanation of your solution here
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -16,7 +27,8 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
-  console.log("");
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mario"))
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
